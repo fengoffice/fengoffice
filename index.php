2ca9de1d05794d5308acc4d6d2ca34307db67822
liAnGjiA <leangjia@qq.com>
2017-01-02 13:32:10 +0800

    1、2017-01-01更新简体中文汉化包；2、更新FengOffice版本到3.4.4.1
    1、2017-01-01更新简体中文汉化包；2、更新FengOffice版本到3.4.4.1

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 
   if(!version_compare(phpversion(), '5.0', '>=')) {
     die('<strong>Installation error:</strong> in order to run Feng Office you need PHP5. Your current PHP version is: ' . phpversion());
